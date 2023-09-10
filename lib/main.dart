b6b3299154e7810655bd74eb126cf4190c012e18
Lakshay-Ahlawat <lakshay6447@gmail.com>
2023-09-10 19:29:17 +0530

    ✨ update logos and loading screen

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Portfolio',
+      title: 'Lakshay Ahlawat',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
